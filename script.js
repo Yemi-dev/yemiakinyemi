0a74b4fc411bbba3da4d57e61045816b3d7f77a2
Akinyemi Oluwayemisi <Yemi@MACs-MacBook-Pro.local>
2022-03-22 00:39:18 +0100

    done

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ document.addEventListener('DOMContentLoaded', setLS);
 document.addEventListener('scroll', btnAppear);
 
 function setLS(){
-    if (getMode === 'dark'){
-        body.classList.add('dark')
-        body.classList.remove('light')
-    } if (getMode === 'light'){
+     if (getMode === 'light'){
         body.classList.add('light')
         body.classList.remove('dark')
+    } if (getMode === 'dark'){
+        body.classList.add('dark')
+        body.classList.remove('light')
     }
     else{
         return;
@@ -38,15 +38,15 @@ if(modeToggle){modeToggle.addEventListener('click', toggleMode);}
 
 function toggleMode(e) {
     e.preventDefault();
-    if ( getMode === 'dark' ){
-        body.classList.remove('dark')
-        body.classList.add('light')
-        localStorage.setItem('mode','light')
+    if ( getMode === 'light' ){
+        body.classList.remove('light')
+        body.classList.add('dark')
+        localStorage.setItem('mode','dark')
        getMode = localStorage.getItem('mode');
     } else {
-        body.classList.add('dark')
-        body.classList.remove('light')
-        localStorage.setItem('mode','dark');
+        body.classList.add('light')
+        body.classList.remove('dark')
+        localStorage.setItem('mode','light');
         getMode = localStorage.getItem('mode');
     }
 }
